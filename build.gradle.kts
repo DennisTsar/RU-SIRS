4ba618aa30d5fe360bdea80102e05952d301d787
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2022-10-18 15:17:57 -0400

    Create multi-module project - with site & local subprojects

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+@Suppress("DSL_SCOPE_VIOLATION") // just to avoid "libs" red underline
+plugins {
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+}
+
+subprojects {
+    repositories {
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
+    }
+}
