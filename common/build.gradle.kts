a48a933800dcb1df8cc404cb1ac525a108375680
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2022-11-22 14:15:51 -0500

    Improve compilation performance

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.targets.js.dukat.DukatTask
+
 @Suppress("DSL_SCOPE_VIOLATION") // just to avoid "libs" red underline
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
@@ -9,11 +11,13 @@ group = "dev.letter"
 kotlin {
     jvm()
     js(IR) {
+        tasks.withType(DukatTask::class) { enabled = false }
         browser()
     }
 
+    @Suppress("UNUSED_VARIABLE")
     sourceSets {
-        val commonMain by getting{
+        val commonMain by getting {
             dependencies {
                 implementation(libs.serialization)
                 implementation(libs.coroutines)
@@ -21,7 +25,7 @@ kotlin {
             }
         }
 
-        val jvmMain by getting{
+        val jvmMain by getting {
             dependencies {
                 implementation(libs.ktor.client.cio)
             }
