f31c3bbcd385deb1ea01a2ed0423ad4d5b86a447
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-01-25 01:56:14 -0500

    Nit: simplify semester constructor

--- common/src/commonMain/kotlin/data/Semester.kt
+++ common/src/commonMain/kotlin/data/Semester.kt
@@ -8,7 +8,7 @@ data class Semester(val type: SemesterType, val year: Int) : Comparable<Semester
         return numValue.compareTo(other.numValue)
     }
 
-    constructor(num: Int) : this(SemesterType.values()[num % 2], num / 2)
+    constructor(num: Int) : this(if (num % 2 == 0) SemesterType.Spring else SemesterType.Fall, num / 2)
 
     val numValue get() = year * 2 + if (type == SemesterType.Spring) 0 else 1
 }
@@ -17,7 +17,7 @@ fun Semester.prev(byAmount: Int = 1) = Semester(numValue - byAmount)
 fun Semester.next(byAmount: Int = 1) = Semester(numValue + byAmount)
 
 enum class SemesterType(val num: Int) {
-    Spring(1), Fall(9); // !! Note that this order matters !!
+    Spring(1), Fall(9);
 
     fun other(): SemesterType = if (this == Spring) Fall else Spring
 }
